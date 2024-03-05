4ae6ea38ec0ce90f53608a09ec02666579eeb92f
spx360 <selahaddin.omer3@gmail.com>
2024-03-05 11:06:15 +0300

    yorum

---
+++ potansiometerXoled.py
@@ -0,0 +1,25 @@
+"""
+       Klasor içerisindeki ssd1306.py dosyası
+       MicroPython cihazına yüklenmelidir.
+"""
+from machine import Pin, SoftI2C, ADC
+import ssd1306
+from time import sleep
+
+pot = ADC(Pin(32))
+pot.width(ADC.WIDTH_10BIT)
+pot.atten(ADC.ATTN_11DB)
+
+
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+
+oled_width = 128
+oled_height = 64
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+while True:
+    pot_res = pot.read()
+    oled.text(str(pot_res), 0, 0)
+    sleep(0.2)
+    oled.show()
+
+
